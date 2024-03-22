fc0b091c3657d7c53499f7c488a8c054fd4830d8
GugeLucas <lucas.gugelmin@gmail.com>
2024-03-22 10:55:59 -0400

    Update

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#adicionar').click(function(event){
+        event.preventDefault(); // Prevenir o comportamento padrão do formulário
+        //obtem valores form
+        var tarefa = $('#tarefa').val();
+        var data = $('#data').val();
+        var hora = $('#hora').val();
+
+        //Cria um item de lista com valores
+        var novoItem = $('<li id="trf">').text('Tarefa: ' 
+        + tarefa + ' - Data: '
+        + data + ' - Hora: ' 
+        + hora );
+
+        //adiciona item a lista
+        $('#listaTarefas').append(novoItem);
+
+        // Limpa o formulario apos a adição
+        $('#tarefa').val('');
+        $('#data').val('');
+        $('#hora').val('');
+
+    });
+    $('#listaTarefas').on('click', 'li', function() {
+        $(this).toggleClass('riscado'); 
+    });
+    
+});
