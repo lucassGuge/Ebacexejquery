e160897a99bbd39e18891e4503bd0f18513ffd81
GugeLucas <lucas.gugelmin@gmail.com>
2024-03-22 10:57:27 -0400

    Update 2

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
     $('#adicionar').click(function(event){
-        event.preventDefault(); // Prevenir o comportamento padrão do formulário
+       
+        // remove comportamento padrão do formulário
+
+        event.preventDefault();        
+
         //obtem valores form
+
         var tarefa = $('#tarefa').val();
         var data = $('#data').val();
         var hora = $('#hora').val();
 
         //Cria um item de lista com valores
+
         var novoItem = $('<li id="trf">').text('Tarefa: ' 
         + tarefa + ' - Data: '
         + data + ' - Hora: ' 
         + hora );
 
         //adiciona item a lista
+
         $('#listaTarefas').append(novoItem);
 
         // Limpa o formulario apos a adição
+        
         $('#tarefa').val('');
         $('#data').val('');
         $('#hora').val('');
